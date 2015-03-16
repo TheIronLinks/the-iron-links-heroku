ae946a1710e98db78abb16d053ea9cf5e6ca0d83
Rachel <BlantonAlmeida@gmail.com>
2015-03-16 08:56:01 -0400

    fixing merge conflicts

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,23 +1,24 @@
 // This is your app.js file.
 (function () {
-  'use strict';
+ 'use strict';
 
-  angular.module('tilAPP', [
-    'ngRoute',
-    'Devise'
-  ])
-  .config(function($httpProvider) {
-     $httpProvider.defaults.headers.common['X-CSRF-Token'] = $("meta[name=csrf-token]").attr("content");
-   })
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'assets/views/index.html'
-      })
-      .when('/newGrad', {
-        templateUrl: 'assets/views/newGrad.html',
-        controller: 'ProfileController as profileCtrl'
-      })
-  })
-  .constant('_',_);
+ angular.module('tilAPP', [
+   'ngRoute',
+   'Devise',
+   'cardStackUI'
+ ])
+ .config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $("meta[name=csrf-token]").attr("content");
+  }])
+ .config(['$routeProvider', function ($routeProvider) {
+   $routeProvider
+     .when('/', {
+       templateUrl: 'assets/views/index.html'
+     })
+     .when('/newGrad', {
+       templateUrl: 'assets/views/newGrad.html',
+       controller: 'ProfileController as profileCtrl'
+     })
+   }])
+ .constant('_',_);
 })();
