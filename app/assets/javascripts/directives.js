def32eef5aeceabd1af1891a104a509b233651e1
Adam <sitzadad@yahoo.com>
2015-03-22 22:54:54 -0400

    basic styling applied to all three card types

--- app/assets/javascripts/directives.js
+++ app/assets/javascripts/directives.js
@@ -4,15 +4,142 @@
 
 //=====================STORING CARD DIRECTIVE IN ROOTSCOPE (minus templateURL)=====================
 
-      .run(['$rootScope', function($rootScope){
+//       .run(['$rootScope', function($rootScope){
+//
+//         $rootScope.card_directive_tmpl = {
+//           restrict: 'E',
+//           scope: {
+//             data: '=',
+//             action: '&'
+//           },
+//           templateUrl: '',
+//           link: function(scope, element, attrs) {
+//
+//             element.find('li[data-trait=\'1\']').attr('data-state', 'selected');
+//
+//             element.find('li[data-trait=\'2\'],li[data-trait=\'3\'],li[data-trait=\'4\']').addClass('invis');
+//
+//             //=====================CARD SLIDER FUNCTIONALITY=====================
+//
+//             element.find('.fa.fa-5x.fa-caret-left,.fa.fa-5x.fa-caret-right').on('click',function(){
+//               var comingFrom = element.find('li[data-state=\'selected\']').data('trait');
+//               var goingTo;
+//               if($(this).attr('rel') === 'left'){
+//                 if(comingFrom === 1){
+//                   goingTo = 4;
+//                 }else{
+//                   goingTo = comingFrom - 1;
+//                 }
+//               }else if($(this).attr('rel') === 'right'){
+//                 if(comingFrom === 4){
+//                   goingTo = 1;
+//                 }else{
+//                   goingTo = comingFrom + 1;
+//                 }
+//               }
+//               element.find('li[data-state=\'selected\']').addClass('invis');
+//               element.find('li[data-state=\'selected\']').attr('data-state', comingFrom);
+//               element.find('li[data-trait='+goingTo+']').removeClass('invis');
+//               element.find('li[data-trait='+goingTo+']').attr('data-state', 'selected');
+//             });
+//
+//             //=====================UNSPREAD CARD=====================
+//
+//             element.find('li').on('click',function(event){
+//               event.stopPropagation();
+//               var comingFrom = element.find('li[data-state=\'selected\']').data('trait');
+//               var goingTo = $(this).closest('li').data('trait');
+//               element.find('li').addClass('invis');
+//               element.find('li[data-state=\'selected\']').attr('data-state', comingFrom);
+//               element.find('li[data-trait='+goingTo+']').attr('data-state', 'selected');
+//               element.find('li[data-trait='+goingTo+']').removeClass('invis');
+//
+//               element.find('li').removeClass('cardSpreadOne cardSpreadTwo cardSpreadThree cardSpreadFour');
+//               element.find('ul').removeClass('cardSpreadStack');
+//               element.find('li').css({'cursor':'auto'});
+//               element.find('.fa.fa-lg,.fa.fa-2x,.fa.fa-5x').css({'display':'inline-block'});
+//             });
+//
+//             //=====================SPREAD CARD=====================
+//
+//             element.find('.fa.fa-2x.fa-ellipsis-v').on('click',function(event){
+//               event.stopPropagation();
+//               console.log('firing spread');
+//               var firstCard = $(this).parent().data('trait');
+//               if(firstCard === 1){
+//                 var secondCard = 2;
+//                 var thirdCard = 3;
+//                 var fourthCard = 4;
+//               }else if(firstCard === 2){
+//                 var secondCard = 3;
+//                 var thirdCard = 4;
+//                 var fourthCard = 1;
+//               }else if(firstCard === 3){
+//                 var secondCard = 4;
+//                 var thirdCard = 1;
+//                 var fourthCard = 2;
+//               }else if(firstCard === 4){
+//                 var secondCard = 1;
+//                 var thirdCard = 2;
+//                 var fourthCard = 3;
+//               }
+//               element.find('li[data-trait='+firstCard+']').addClass('cardSpreadOne');
+//               element.find('li[data-trait='+secondCard+']').addClass('cardSpreadTwo');
+//               element.find('li[data-trait='+thirdCard+']').addClass('cardSpreadThree');
+//               element.find('li[data-trait='+fourthCard+']').addClass('cardSpreadFour');
+//               element.find('ul').addClass('cardSpreadStack');
+//               element.find('li').removeClass('invis');
+//               element.find('.fa.fa-lg,.fa.fa-2x,.fa.fa-5x').css({'display':'none'});
+//               element.find('li').css({'cursor':'pointer'});
+//
+//             });
+//
+//             //=====================CARD DETAIL FUNCTIONALITY=====================
+//
+//             element.find('.fa.fa-lg.fa-flip-horizontal.fa-expand').on('click',function(){
+//               //event.stopPropagation();
+//
+//             });
+//
+//             }
+//           };
+//       }])
+//
+// //=====================GRAD CARD DIRECTIVE=====================
+//
+//       .directive('gradCardDirective', function($location, $rootScope) {
+//         var return_object = $rootScope.card_directive_tmpl;
+//         return_object.templateUrl = 'assets/directiveTemplates/cardStack.graduate.directive.html';
+//         return return_object;
+//       })
+//
+// //=====================EMPL CARD DIRECTIVE=====================
+//
+//       .directive('emplCardDirective', function($location, $rootScope) {
+//         var return_object = $rootScope.card_directive_tmpl;
+//         return_object.templateUrl = 'assets/directiveTemplates/cardStack.employer.directive.html';
+//         return return_object;
+//       })
+//
+// //=====================JOB CARD DIRECTIVE=====================
+//
+//       .directive('jobCardDirective', function($location, $rootScope) {
+//         var return_object = $rootScope.card_directive_tmpl;
+//         return_object.templateUrl = 'assets/directiveTemplates/cardStack.job.directive.html';
+//         return return_object;
+//       })
 
-        $rootScope.card_directive_tmpl = {
+//=====================GRAD CARD DIRECTIVE=====================
+
+      .directive('gradCardDirective', function($location, $rootScope) {
+
+        return {
           restrict: 'E',
           scope: {
             data: '=',
             action: '&'
           },
-          templateUrl: '',
+          templateUrl: 'assets/directiveTemplates/cardStack.graduate.directive.html',
           link: function(scope, element, attrs) {
 
             element.find('li[data-trait=\'1\']').attr('data-state', 'selected');
@@ -103,35 +230,236 @@
 
             }
           };
-      }])
 
-//=====================GRAD CARD DIRECTIVE=====================
-
-      .directive('gradCardDirective', function($location, $rootScope) {
-        var return_object = $rootScope.card_directive_tmpl;
-        return_object.templateUrl = 'assets/directiveTemplates/cardStack.graduate.directive.html';
-        return return_object;
       })
 
 //=====================EMPL CARD DIRECTIVE=====================
 
       .directive('emplCardDirective', function($location, $rootScope) {
-        var return_object = $rootScope.card_directive_tmpl;
-        return_object.templateUrl = 'assets/directiveTemplates/cardStack.employer.directive.html';
-        return return_object;
+
+        return {
+          restrict: 'E',
+          scope: {
+            data: '=',
+            action: '&'
+          },
+          templateUrl: 'assets/directiveTemplates/cardStack.employer.directive.html',
+          link: function(scope, element, attrs) {
+
+            element.find('li[data-trait=\'1\']').attr('data-state', 'selected');
+
+            element.find('li[data-trait=\'2\'],li[data-trait=\'3\'],li[data-trait=\'4\']').addClass('invis');
+
+            //=====================CARD SLIDER FUNCTIONALITY=====================
+
+            element.find('.fa.fa-5x.fa-caret-left,.fa.fa-5x.fa-caret-right').on('click',function(){
+              var comingFrom = element.find('li[data-state=\'selected\']').data('trait');
+              var goingTo;
+              if($(this).attr('rel') === 'left'){
+                if(comingFrom === 1){
+                  goingTo = 4;
+                }else{
+                  goingTo = comingFrom - 1;
+                }
+              }else if($(this).attr('rel') === 'right'){
+                if(comingFrom === 4){
+                  goingTo = 1;
+                }else{
+                  goingTo = comingFrom + 1;
+                }
+              }
+              element.find('li[data-state=\'selected\']').addClass('invis');
+              element.find('li[data-state=\'selected\']').attr('data-state', comingFrom);
+              element.find('li[data-trait='+goingTo+']').removeClass('invis');
+              element.find('li[data-trait='+goingTo+']').attr('data-state', 'selected');
+            });
+
+            //=====================UNSPREAD CARD=====================
+
+            element.find('li').on('click',function(event){
+              event.stopPropagation();
+              var comingFrom = element.find('li[data-state=\'selected\']').data('trait');
+              var goingTo = $(this).closest('li').data('trait');
+              element.find('li').addClass('invis');
+              element.find('li[data-state=\'selected\']').attr('data-state', comingFrom);
+              element.find('li[data-trait='+goingTo+']').attr('data-state', 'selected');
+              element.find('li[data-trait='+goingTo+']').removeClass('invis');
+
+              element.find('li').removeClass('cardSpreadOne cardSpreadTwo cardSpreadThree cardSpreadFour');
+              element.find('ul').removeClass('cardSpreadStack');
+              element.find('li').css({'cursor':'auto'});
+              element.find('.fa.fa-lg,.fa.fa-2x,.fa.fa-5x').css({'display':'inline-block'});
+            });
+
+            //=====================SPREAD CARD=====================
+
+            element.find('.fa.fa-2x.fa-ellipsis-v').on('click',function(event){
+              event.stopPropagation();
+              console.log('firing spread');
+              var firstCard = $(this).parent().data('trait');
+              if(firstCard === 1){
+                var secondCard = 2;
+                var thirdCard = 3;
+                var fourthCard = 4;
+              }else if(firstCard === 2){
+                var secondCard = 3;
+                var thirdCard = 4;
+                var fourthCard = 1;
+              }else if(firstCard === 3){
+                var secondCard = 4;
+                var thirdCard = 1;
+                var fourthCard = 2;
+              }else if(firstCard === 4){
+                var secondCard = 1;
+                var thirdCard = 2;
+                var fourthCard = 3;
+              }
+              element.find('li[data-trait='+firstCard+']').addClass('cardSpreadOne');
+              element.find('li[data-trait='+secondCard+']').addClass('cardSpreadTwo');
+              element.find('li[data-trait='+thirdCard+']').addClass('cardSpreadThree');
+              element.find('li[data-trait='+fourthCard+']').addClass('cardSpreadFour');
+              element.find('ul').addClass('cardSpreadStack');
+              element.find('li').removeClass('invis');
+              element.find('.fa.fa-lg,.fa.fa-2x,.fa.fa-5x').css({'display':'none'});
+              element.find('li').css({'cursor':'pointer'});
+
+            });
+
+            //=====================CARD DETAIL FUNCTIONALITY=====================
+
+            element.find('.fa.fa-lg.fa-flip-horizontal.fa-expand').on('click',function(){
+              //event.stopPropagation();
+
+            });
+
+            }
+          };
+
       })
 
 //=====================JOB CARD DIRECTIVE=====================
 
       .directive('jobCardDirective', function($location, $rootScope) {
-        var return_object = $rootScope.card_directive_tmpl;
-        return_object.templateUrl = 'assets/directiveTemplates/cardStack.job.directive.html';
-        return return_object;
+
+        return {
+          restrict: 'E',
+          scope: {
+            data: '=',
+            action: '&'
+          },
+          templateUrl: 'assets/directiveTemplates/cardStack.job.directive.html',
+          link: function(scope, element, attrs) {
+
+            element.find('li[data-trait=\'1\']').attr('data-state', 'selected');
+
+            element.find('li[data-trait=\'2\'],li[data-trait=\'3\'],li[data-trait=\'4\']').addClass('invis');
+
+            //=====================CARD SLIDER FUNCTIONALITY=====================
+
+            element.find('.fa.fa-5x.fa-caret-left,.fa.fa-5x.fa-caret-right').on('click',function(){
+              var comingFrom = element.find('li[data-state=\'selected\']').data('trait');
+              var goingTo;
+              if($(this).attr('rel') === 'left'){
+                if(comingFrom === 1){
+                  goingTo = 4;
+                }else{
+                  goingTo = comingFrom - 1;
+                }
+              }else if($(this).attr('rel') === 'right'){
+                if(comingFrom === 4){
+                  goingTo = 1;
+                }else{
+                  goingTo = comingFrom + 1;
+                }
+              }
+              element.find('li[data-state=\'selected\']').addClass('invis');
+              element.find('li[data-state=\'selected\']').attr('data-state', comingFrom);
+              element.find('li[data-trait='+goingTo+']').removeClass('invis');
+              element.find('li[data-trait='+goingTo+']').attr('data-state', 'selected');
+            });
+
+            //=====================UNSPREAD CARD=====================
+
+            element.find('li').on('click',function(event){
+              event.stopPropagation();
+              var comingFrom = element.find('li[data-state=\'selected\']').data('trait');
+              var goingTo = $(this).closest('li').data('trait');
+              element.find('li').addClass('invis');
+              element.find('li[data-state=\'selected\']').attr('data-state', comingFrom);
+              element.find('li[data-trait='+goingTo+']').attr('data-state', 'selected');
+              element.find('li[data-trait='+goingTo+']').removeClass('invis');
+
+              element.find('li').removeClass('cardSpreadOne cardSpreadTwo cardSpreadThree cardSpreadFour');
+              element.find('ul').removeClass('cardSpreadStack');
+              element.find('li').css({'cursor':'auto'});
+              element.find('.fa.fa-lg,.fa.fa-2x,.fa.fa-5x').css({'display':'inline-block'});
+            });
+
+            //=====================SPREAD CARD=====================
+
+            element.find('.fa.fa-2x.fa-ellipsis-v').on('click',function(event){
+              event.stopPropagation();
+              console.log('firing spread');
+              var firstCard = $(this).parent().data('trait');
+              if(firstCard === 1){
+                var secondCard = 2;
+                var thirdCard = 3;
+                var fourthCard = 4;
+              }else if(firstCard === 2){
+                var secondCard = 3;
+                var thirdCard = 4;
+                var fourthCard = 1;
+              }else if(firstCard === 3){
+                var secondCard = 4;
+                var thirdCard = 1;
+                var fourthCard = 2;
+              }else if(firstCard === 4){
+                var secondCard = 1;
+                var thirdCard = 2;
+                var fourthCard = 3;
+              }
+              element.find('li[data-trait='+firstCard+']').addClass('cardSpreadOne');
+              element.find('li[data-trait='+secondCard+']').addClass('cardSpreadTwo');
+              element.find('li[data-trait='+thirdCard+']').addClass('cardSpreadThree');
+              element.find('li[data-trait='+fourthCard+']').addClass('cardSpreadFour');
+              element.find('ul').addClass('cardSpreadStack');
+              element.find('li').removeClass('invis');
+              element.find('.fa.fa-lg,.fa.fa-2x,.fa.fa-5x').css({'display':'none'});
+              element.find('li').css({'cursor':'pointer'});
+
+            });
+
+            //=====================CARD DETAIL FUNCTIONALITY=====================
+
+            element.find('.fa.fa-lg.fa-flip-horizontal.fa-expand').on('click',function(){
+              //event.stopPropagation();
+
+            });
+
+            }
+          };
+
       })
 
 //=====================GRAD DETAIL DIRECTIVE=====================
 
       .directive('gradDetailDirective', function() {
+
+        return {
+          restrict: 'E',
+          scope: {
+            data: '=',
+            return: '&',
+            close: '&'
+          },
+          templateUrl: 'assets/directiveTemplates/cardDetail.graduate.directive.html',
+          link: function(scope, element, attrs) {
+
+
+
+          }
+
+        };
 
       })
 
@@ -166,6 +494,22 @@
 //=====================JOB DETAIL DIRECTIVE=====================
 
       .directive('jobDetailDirective', function() {
+
+        return {
+          restrict: 'E',
+          scope: {
+            data: '=',
+            return: '&',
+            close: '&'
+          },
+          templateUrl: 'assets/directiveTemplates/cardDetail.job.directive.html',
+          link: function(scope, element, attrs) {
+
+
+
+          }
+
+        };
 
       })
 
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -25,15 +25,15 @@
         controller: 'ProfileController as profileCtrl'
       })
       .when('/graduates', {
-        templateUrl: 'assets/views/cardStackViews/alumniCardView.html',
+        templateUrl: 'assets/views/cardStackViews/gradCardView.html',
         controller: 'CardController as cardCtrl'
       })
       .when('/employers', {
-        templateUrl: 'assets/views/cardStackViews/employersCardView.html',
+        templateUrl: 'assets/views/cardStackViews/emplCardView.html',
         controller: 'CardController as cardCtrl'
       })
       .when('/jobs', {
-        templateUrl: 'assets/views/cardStackViews/jobsCardView.html',
+        templateUrl: 'assets/views/cardStackViews/jobCardView.html',
         controller: 'CardController as cardCtrl'
       })
       .when('/not-found', {
